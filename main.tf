6ce07363980ae78871af3476e557022105fb0cdf
unknown <jt1.singh@gmail.com>
2026-01-03 17:37:30 +0530

    Terraform CloudWatch automation

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+locals {
+    alarms = flatten ([
+      for metric in var.metric_name : [
+        for instanceid in var.InsId : {
+          metric = metric
+          instanceid = instanceid
+        }
+      ]
+    ])
+}
+
+resource "aws_cloudwatch_metric_alarm" "ec2alarms" {
+  #for_each = { for metrics in var.metric : metrics => metrics }
+  for_each = { for alarms in local.alarms : "${alarms.metric}-${alarms.instanceid}" => alarms }
+  alarm_name = format("High-EC2-Utilization-Alarm-for-%s-monitoring-and-instance-is-%s", each.value.metric, each.value.instanceid)
+  evaluation_periods = 1
+  datapoints_to_alarm = 1
+  comparison_operator = "LessThanThreshold"
+  namespace = "AWS/EC2"
+  metric_name = each.value.metric
+  period = 60
+  statistic = "Minimum"
+  threshold = 5
+  alarm_description = format("This is being used for EC2 %s monitoring", each.value.metric)
+  alarm_actions = ["arn:aws:sns:us-east-1:707318434536:AWS-ALARM-JITEN"]
+  dimensions = {
+    InstanceId = each.value.instanceid
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable metric_name {
+    type = list(string)
+    default = ["CPUUtilization", "DiskWriteOps"]
+}
+
+variable "InsId" {
+    type = list(string)
+    default = ["i-0307cd4b4265c1aa0", "i-03d501112b3d30c7b"]
+  
+}
+
+variable "alarms" {
+    type = map(string)
+    default = {}
+}
